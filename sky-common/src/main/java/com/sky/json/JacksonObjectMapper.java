25e68b2bb5fd642bd93a1929c481fd230499a080
duanyu <2895580172@qq.com>
2025-08-28 20:03:54 +0800

    新增员工相关代码

--- sky-common/src/main/java/com/sky/json/JacksonObjectMapper.java
+++ sky-common/src/main/java/com/sky/json/JacksonObjectMapper.java
@@ -34,7 +34,7 @@ public class JacksonObjectMapper extends ObjectMapper {
         //收到未知属性时不报异常
         this.configure(FAIL_ON_UNKNOWN_PROPERTIES, false);
 
-        //反序列化时，属性不存在的兼容处理
+        //反序列化时，属性不存在的兼容处理ion e) {
         this.getDeserializationConfig().withoutFeatures(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
 
         SimpleModule simpleModule = new SimpleModule()
